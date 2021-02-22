6b80679c720d4f7cf0cf18fadc8828ef8060b530
oldirony <raffaele.abramini@gmail.com>
2021-02-22 10:31:07 +0000

    :construction: Work in progress.

---
+++ public/demo.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import reactDom  from 'react-dom'
+import { HtmlInIframe } from '../src'
+
+reactDom.render(<HtmlInIframe />, document.getElementById("app"));
---
+++ src/HtmlInIframe.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const HtmlInIframe = () => {
+    return (
+        <div>hey</div>
+    )
+};
