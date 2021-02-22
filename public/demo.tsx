d4f6c10aa5487d12a5657d2c58382aa3125c1c16
oldirony <raffaele.abramini@gmail.com>
2021-02-22 11:23:54 +0000

    :construction: Work in progress.

--- public/demo.tsx
+++ public/demo.tsx
@@ -1,5 +1,23 @@
 import React from "react";
-import reactDom  from 'react-dom'
-import { HtmlInIframe } from '../src'
+import reactDom from "react-dom";
+import { HtmlInIframe } from "../src";
+// @ts-ignore
+import html from "./unsafeHtml.html";
 
-reactDom.render(<HtmlInIframe />, document.getElementById("app"));
+const myUnsafeHtml = html;
+
+const customcss = `
+    body {
+    font-family: sans-serif;
+    }
+`;
+
+reactDom.render(
+  <div>
+    <h2>here's a normal iframe</h2>
+    <iframe src={"data:text/html," + myUnsafeHtml} />
+    <h2>here's our fancy iframe</h2>
+    <HtmlInIframe unsafeHtml={myUnsafeHtml} customStyle={customcss as any} />
+  </div>,
+  document.getElementById("app")
+);
--- src/HtmlInIframe.tsx
+++ src/HtmlInIframe.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const HtmlInIframe = () => {
-    return (
-        <div>hey</div>
-    )
+export interface HtmlInIframeProps {
+  customStyle?: string;
+  unsafeHtml: string;
+  heightCorrection: boolean;
+  heightCorrectionOnResize: boolean;
+  debounceResize: boolean;
+}
+
+const renderResizeScript = (props: HtmlInIframeProps) => {
+  if (!props.heightCorrection) {
+    return "";
+  }
+  let output = `parent.postMessage(document.documentElement.scrollHeight, "${location.href}");`;
+  if (!props.heightCorrectionOnResize) {
+    return output;
+  }
+
+  if (props.debounceResize) {
+    output += `
+        function debounce(func, wait, immediate) {
+            var timeout;
+            return function() {
+                var context = this, args = arguments;
+                var later = function() {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                var callNow = immediate && !timeout;
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+                if (callNow) func.apply(context, args);
+            };
+        };
+        const handleResize = debounce(() => {
+            parent.postMessage(document.documentElement.scrollHeight, "${location.href}")
+        }, 250);
+`;
+  } else {
+    output += `
+        const handleResize = () => {
+            parent.postMessage(document.documentElement.scrollHeight, "${location.href}")
+        };
+    `;
+  }
+
+  return output + 'window.addEventListener("resize", handleResize);';
 };
+
+const HtmlInIframe = (props: HtmlInIframeProps) => {
+  const styleTag = `<style>${props.customStyle || ""}</style>`;
+  const heightListener = `<script>${renderResizeScript(props)}</script>`;
+
+  const src = `data:text/html,${styleTag}${props.unsafeHtml}${heightListener}`;
+  const [height, setHeight] = useState(100);
+
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      (e) => {
+        let height: number;
+        try {
+          height = Number(JSON.parse(e.data));
+        } catch (e) {
+          console.error("cannot parse iframe message");
+        }
+
+        setHeight(height);
+      },
+      false
+    );
+  });
+  return (
+    <iframe
+      style={{ border: "none", width: "100%" }}
+      src={src}
+      height={height}
+    />
+  );
+};
+
+HtmlInIframe.defaultProps = {
+  heightCorrection: true,
+  heightCorrectionOnResize: true,
+  debounceResize: true,
+};
+
+export { HtmlInIframe };
