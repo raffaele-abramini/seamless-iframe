43a0ef3683c4ea7d8d61b2c6d8acb1212d392ee2
oldirony <raffaele.abramini@gmail.com>
2021-03-08 18:13:10 +0000

    :bug: Add demo page

--- public/assets/demo.tsx
+++ public/assets/demo.tsx
@@ -1,233 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import reactDom from "react-dom";
+import { Config, sanitize } from "dompurify";
 import { SeamlessIframe } from "seamless-iframe";
-import externalHtml from "../examples/externalHtml.html";
-import longHtml from "../examples/longHtml.html";
-import unsafeHtml from "../examples/unsafehtml.html";
-import unsafeHtml2 from "../examples/unsafehtml2.html";
-import htmlWithWeirdChars from "../examples/htmlWithWeirdChars.html";
-import extremelyLongHtml from "../examples/extremelyLongHtml.html";
 
-const customCss = `
-  body {
-    margin: 0;
-    padding: 0;
-  }
-`;
-const customCss2 = `
-  body {
-    margin: 0;
-    padding: 0;
-    color: slategray;
-  }
-`;
-
-const visibleSections = {
-  a: true,
-  a1: true,
-  a2: true,
-  b: true,
-  c: true,
-  d: true,
-  e: true,
-  f: false,
+const c: Config = {
+  FORCE_BODY: true,
 };
 
-const visibleContent = {
-  original: true,
-  seamless: true,
-};
-
-const [isStolen] = location?.pathname.match(/stolen/g) || [];
-
-if (isStolen) {
-  reactDom.render(
-    <>
-      <h1>Hi, your data COULD HAVE BEEN stolen.</h1>
-      <a href="/">Go back to safety!</a>
-    </>,
-    document.getElementById("app")
-  );
-}
-
-localStorage.setItem("secret", "mySecretCode");
-document.cookie = "mySecretCookie";
-
-if (!isStolen) {
-  reactDom.render(
-    <main>
-      {visibleSections.a && (
-        <section>
-          <h2>Automatically gets styles from parent window</h2>
-          <div className="two-cols">
-            <div>
-              <h3>Default iframe</h3>
-              <iframe src={"data:text/html," + externalHtml} />
-            </div>
-            <div>
-              <h3>Seamless iframe</h3>
-              <SeamlessIframe sanitizedHtml={externalHtml} />
-            </div>
-          </div>
-        </section>
-      )}
-      {visibleSections.a1 && (
-        <section>
-          <h2>Receives custom styles</h2>
-          <div className="two-cols">
-            <div>
-              <h3>Default iframe</h3>
-              <iframe src={"data:text/html," + externalHtml} />
-            </div>
-            <div>
-              <h3>Seamless iframe</h3>
-              <SeamlessIframe
-                sanitizedHtml={externalHtml}
-                customStyle={customCss2}
-              />
-              <h4>Provided styles</h4>
-              <pre>{customCss2}</pre>
-            </div>
-          </div>
-        </section>
-      )}
-      {visibleSections.a2 && (
-        <section>
-          <h2>Ignores parent style, just get custom style</h2>
-          <div className="two-cols">
-            <div>
-              <h3>Default iframe</h3>
-              <iframe src={"data:text/html," + externalHtml} />
-            </div>
-            <div>
-              <h3>Seamless iframe</h3>
-              <SeamlessIframe
-                sanitizedHtml={externalHtml}
-                customStyle={customCss2}
-                inheritParentStyle={false}
-              />
-              <h4>Provided styles</h4>
-              <pre>{customCss2}</pre>
-            </div>
-          </div>
-        </section>
-      )}
-      {visibleSections.b && (
-        <section>
-          <h2>Automatically resizes its height</h2>
-
-          <div className="two-cols">
-            <div>
-              <h3>Default iframe</h3>
-              <iframe src={"data:text/html," + longHtml} />
-            </div>
-            <div>
-              <h3>Seamless iframe</h3>
-              <SeamlessIframe
-                sanitizedHtml={longHtml}
-                customStyle={customCss}
-              />
-            </div>
-          </div>
-        </section>
-      )}
-      {visibleSections.c && (
-        <section>
-          <h2>Prevents injected content from submitting forms</h2>
-          <div className="two-cols">
-            {visibleContent.original && (
-              <div>
-                <h3>HTML directly injected in page</h3>
-                <div dangerouslySetInnerHTML={{ __html: unsafeHtml }} />
-              </div>
-            )}
-            {visibleContent.seamless && (
-              <div>
-                <h3>Seamless iframe</h3>
-                <SeamlessIframe
-                  sanitizedHtml={unsafeHtml}
-                  customStyle={customCss}
-                />
-              </div>
-            )}
-          </div>
-        </section>
-      )}
-
-      {visibleSections.d && (
-        <section>
-          <h2>
-            Prevents rendered html from accessing location, cookies and storage
-          </h2>
+const App = () => {
+  const [content, setContent] = useState(`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Seamless Iframe - Demo</title>
+    <link rel="stylesheet" href="assets/style.css" />
+  </head>
+  <body>   
+    <h1>Hello there!</h1>
+    <p>Here's some html content.</p>
+    <table>
+        <tr><td>1</td><td>2</td></tr>
+        <tr><td>4</td><td>4</td></tr>
+    </table>
+    <img src="https://dummyimage.com/600x400/000/fff" alt="">
+  </body>
+</html>`);
+  return (
+    <div>
+      <h1>Hey! Play around here</h1>
+      <div className="two-cols">
+        <div>
+          <textarea
+            defaultValue={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <div>
           <p>
-            For this example we are storing a localStorage key of "mySecretCode"
-            and a cookie of value "test"
+            Here's some outer content to show how <strong>seamless</strong> this
+            iframe here.
           </p>
-          <div className="two-cols">
-            {visibleContent.original && (
-              <div>
-                <h3>HTML directly injected in page</h3>
-                <div dangerouslySetInnerHTML={{ __html: unsafeHtml2 }} />
-              </div>
-            )}
-            {visibleContent.seamless && (
-              <div>
-                <h3>Seamless iframe</h3>
-                <SeamlessIframe
-                  sanitizedHtml={unsafeHtml2}
-                  customStyle={customCss}
-                />
-              </div>
-            )}
-          </div>
-        </section>
-      )}
-
-      {visibleSections.e && (
-        <section>
-          <h2>Works with weird quotes and chars</h2>
-          <div className="two-cols">
-            {visibleContent.original && (
-              <div>
-                <h3>Default iframe</h3>
-                <iframe src={"data:text/html," + htmlWithWeirdChars} />
-              </div>
-            )}
-            {visibleContent.seamless && (
-              <div>
-                <h3>Seamless iframe</h3>
-                <SeamlessIframe
-                  sanitizedHtml={htmlWithWeirdChars}
-                  customStyle={customCss}
-                />
-              </div>
-            )}
-          </div>
-        </section>
-      )}
-      {visibleSections.f && (
-        <section>
-          <h2>Extremely long html (1.6Mb)</h2>
-          <div className="two-cols">
-            {visibleContent.original && (
-              <div>
-                <h3>Default iframe</h3>
-                <iframe src={"data:text/html," + extremelyLongHtml} />
-              </div>
-            )}
-            {visibleContent.seamless && (
-              <div>
-                <h3>Seamless iframe</h3>
-                <SeamlessIframe
-                  sanitizedHtml={extremelyLongHtml}
-                  customStyle={customCss}
-                />
-              </div>
-            )}
-          </div>
-        </section>
-      )}
-    </main>,
-    document.getElementById("app")
+          <SeamlessIframe
+            sanitizedHtml={sanitize(content, c) as string}
+            customStyle={`body {margin:0;padding:0;}`}
+          />
+          <p>
+            Here's some <i>other</i> outer content to show how{" "}
+            <strong>seamless</strong> this iframe here.
+          </p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+reactDom.render(<App />, document.getElementById("app"));
