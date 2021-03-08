6747bd828ba84e7a9c88aea11aabfa946910cc38
oldirony <raffaele.abramini@gmail.com>
2021-03-08 17:22:36 +0000

    :bug: Pages

--- public/assets/demo.tsx
+++ public/assets/demo.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import reactDom from "react-dom";
 import { SeamlessIframe } from "seamless-iframe";
-import externalHtml from "./examples/externalHtml.html";
-import longHtml from "./examples/longHtml.html";
-import unsafeHtml from "./examples/unsafehtml.html";
-import unsafeHtml2 from "./examples/unsafehtml2.html";
-import htmlWithWeirdChars from "./examples/htmlWithWeirdChars.html";
-import extremelyLongHtml from "./examples/extremelyLongHtml.html";
+import externalHtml from "../examples/externalHtml.html";
+import longHtml from "../examples/longHtml.html";
+import unsafeHtml from "../examples/unsafehtml.html";
+import unsafeHtml2 from "../examples/unsafehtml2.html";
+import htmlWithWeirdChars from "../examples/htmlWithWeirdChars.html";
+import extremelyLongHtml from "../examples/extremelyLongHtml.html";
 
 const customCss = `
   body {
