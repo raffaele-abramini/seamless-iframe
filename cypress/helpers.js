6a0c8642ab446458dd246aeb55e6c58c153e1044
Raffaele Abramini <rabramini@twilio.com>
2021-03-13 17:49:19 +0000

    ✅ Add initial batch of e2e (#1)

---
+++ cypress/helpers.js
@@ -0,0 +1,24 @@
+export const IFRAME_SELECTOR = "[data-seamless-iframe-container] iframe";
+
+export const getIframeDocument = () => {
+  return cy
+    .get(IFRAME_SELECTOR)
+    .wait(300)
+    .its("0.contentDocument")
+    .should("exist");
+};
+
+export const getIframeWindow = () => {
+  return cy
+    .get(IFRAME_SELECTOR)
+    .wait(300)
+    .its("0.contentWindow")
+    .should("exist");
+};
+
+export const getIframeBody = () => {
+  return getIframeDocument()
+    .its("body")
+    .should("not.be.undefined")
+    .then(cy.wrap);
+};
---
+++ cypress/integration/getsCustomStyles.spec.js
@@ -0,0 +1,28 @@
+import { getIframeWindow, getIframeBody } from "../helpers";
+describe("Gets custom styles", () => {
+  beforeEach("visits the app", () => {
+    cy.visit("/section/a2");
+  });
+
+  it("should ignore parents styles", () => {
+    getIframeWindow().then((w) => {
+      getIframeBody()
+        .find("h1")
+        .then((h1) => {
+          expect(w.getComputedStyle(h1[0]).fontFamily).not.equal(
+            '"helvetica neue", helvetica, sans-serif'
+          );
+        });
+    });
+  });
+
+  it("should set the title to gray, as per custom style", () => {
+    getIframeWindow().then((w) => {
+      getIframeBody()
+        .find("h1")
+        .then((h1) => {
+          expect(w.getComputedStyle(h1[0]).color).equal("rgb(112, 128, 144)");
+        });
+    });
+  });
+});
